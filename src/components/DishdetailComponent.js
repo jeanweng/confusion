f929f84ad207d7d1d0c9bc8dff6208994e8f995e
zhiyingweng <zhiyingweng@brandeis.edu>
2021-02-03 22:52:42 -0500

    initial commit

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+
+	
+function RenderDish({dish}){
+	return (
+		<Card>
+			<CardImg width="100%" src={dish.image} alt={dish.name} />
+			<CardBody>
+				<CardTitle>{dish.name}</CardTitle>
+				<CardText>{dish.description}</CardText>
+			</CardBody>
+		</Card>
+	);
+
+}
+
+function RenderComments({comments}){
+	if(comments != null){
+		const items = comments.map(item => {
+			return (
+				<ul className="list-unstyled">
+					<li>{item.comment}</li>
+					<li>{item.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(item.date)))}</li>
+				</ul>
+			);
+		})
+		return (
+			<div>
+				<h4>Comments</h4>
+				{items}
+			</div>
+		);
+	}
+	else{
+		return <div></div>;
+	}
+}
+
+const DishDetail = (props) =>{
+	if(props.dish != null){
+		return (
+			<div className="container">
+				<div className="row">
+					<Breadcrumb>
+						<BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+						<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+						<h3>{props.dish.name}</h3>
+						<hr />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12 col-md-5 m-1">
+						<RenderDish dish={props.dish} />
+					</div>
+					<div className="col-12 col-md-5 m-1">
+						<RenderComments comments={props.comments} />
+					</div>
+				</div>
+			</div>
+		);
+	}
+	else{
+		return <div></div>;
+	}
+}
+
+export default DishDetail;
